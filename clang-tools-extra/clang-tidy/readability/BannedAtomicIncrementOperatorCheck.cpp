f60dc0912b7754d66c9dce2805a8a6eae0a1b083
AJ Heller <hork@google.com>
2023-06-07 10:04:23 -0700

    cleanup

--- clang-tools-extra/clang-tidy/readability/BannedAtomicIncrementOperatorCheck.cpp
+++ clang-tools-extra/clang-tidy/readability/BannedAtomicIncrementOperatorCheck.cpp
@@ -19,22 +19,11 @@ namespace tidy {
 namespace readability {
 
 void BannedAtomicIncrementOperatorCheck::registerMatchers(MatchFinder *Finder) {
-  // FIXME: Add matchers.
-  // Finder->addMatcher(functionDecl().bind("x"), this);
   Finder->addMatcher(unaryOperator().bind("op"), this);
 }
 
 void BannedAtomicIncrementOperatorCheck::check(
     const MatchFinder::MatchResult &Result) {
-  // FIXME: Add callback implementation.
-  // const auto *MatchedDecl = Result.Nodes.getNodeAs<FunctionDecl>("x");
-  // if (!MatchedDecl->getIdentifier() ||
-  // MatchedDecl->getName().startswith("awesome_"))
-  //   return;
-  // diag(MatchedDecl->getLocation(), "function %0 is insufficiently awesome")
-  //     << MatchedDecl;
-  // diag(MatchedDecl->getLocation(), "insert 'awesome'", DiagnosticIDs::Note)
-  //     << FixItHint::CreateInsertion(MatchedDecl->getLocation(), "awesome_");
   const auto *MatchedOp = Result.Nodes.getNodeAs<UnaryOperator>("op");
   if (MatchedOp->isIncrementDecrementOp() &&
       MatchedOp->getType()->isAtomicType()) {
